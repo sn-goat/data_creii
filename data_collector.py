fa48db54df6bac481d0e996ba04fca2527430d16
Sn-goat <sergile-junior.domguia-nyouvop@polymtl.ca>
2024-06-03 10:44:45 -0400

    playwright

--- data_collector.py
+++ data_collector.py
@@ -1,8 +1,10 @@
 from playwright.sync_api import sync_playwright
+import time
 
 with sync_playwright() as p:
-    browser = p.chromium.launch()
+    browser = p.chromium.launch(headless=False)
     page = browser.new_page()
-    page.goto("http://playwright.dev")
-    print(page.title())
+    page.goto("https://www.facebook.com")
+    time.sleep(5)
+
     browser.close()
