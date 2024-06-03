93a7a5af8b6b8f3cfcf560dc705807877c1c49ea
Sn-goat <sergile-junior.domguia-nyouvop@polymtl.ca>
2024-06-03 14:38:50 -0400

    done data collector facebook

--- data_collector.py
+++ data_collector.py
@@ -46,8 +46,4 @@ def main():
     data_collector_facebook()
 
 if __name__ == '__main__':
-    schedule.every(1).minutes.do(main)
-
-    while True:
-        schedule.run_pending()
-        time.sleep(1)
+    main()
